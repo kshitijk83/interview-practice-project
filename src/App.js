f2c0a5bfea572684ea961e4da95999777a4cacd5
kshitijk83 <kkshitiz89@gmail.com>
2023-08-06 22:13:31 +0530

    flight booker

--- src/App.js
+++ src/App.js
@@ -1,121 +1,63 @@
 import React from 'react';
 import './style.css';
 
-const leftList = ['HTML', 'JavaScript', 'CSS', 'TypeScript'];
-
-const rightList = ['React', 'Angular', 'Vue', 'Svelte'];
-
-const convertToMap = (list) => {
-  const map = new Map();
-  list.forEach((item) => {
-    map.set(item, false);
-  });
-  return map;
-};
-
-const CheckBoxItem = ({ label, checked, onChange }) => {
-  const id = React.useId();
-  return (
-    <div className="transfer-list__section__items__item">
-      <input type="checkbox" id={id} checked={checked} onChange={onChange} />
-      <label htmlFor={id}>{label}</label>
-    </div>
-  );
-};
-
-const ItemList = ({ items, setItems }) => {
-  return (
-    <div className="transfer-list__section">
-      <ul className="transfer-list__section__items">
-        {Array.from(items.entries()).map(([label, isChecked]) => (
-          <li key={label}>
-            <CheckBoxItem
-              label={label}
-              checked={isChecked}
-              onChange={() => {
-                const newMap = new Map(items);
-                newMap.set(label, !items.get(label));
-                setItems(newMap);
-              }}
-            />
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-};
-
-const hasNoItemSelected = (items) => {
-  const x = Array.from(items.values()).filter((val) => Boolean(val));
-  return x.length === 0;
-};
-
-const transferAllItems = (src, setSrc, dest, setDest) => {
-  setDest(new Map([...dest, ...src]));
-  setSrc(new Map());
-};
-
-const transferSelectedItem = (src, setSrc, dest, setDest) => {
-  const newSrc = new Map(src);
-  const newDest = new Map(dest);
-  Array.from(newSrc.entries()).forEach(([label, checked]) => {
-    if (!checked) {
-      return;
-    }
-    newDest.set(label, false);
-    newSrc.delete(label);
-  });
-
-  setSrc(newSrc);
-  setDest(newDest);
+const formatDate = (dateObj) => {
+  const year = dateObj.getFullYear();
+  const month = (dateObj.getMonth() + 1).toString().padStart(2, '0');
+  const date = dateObj.getDate().toString().padStart(2, '0');
+  return [year, month, date].join('-'); //YYYY-MM-DD
 };
 
 export default function App() {
-  const [left, setLeft] = React.useState(convertToMap(leftList));
-  const [right, setRight] = React.useState(convertToMap(rightList));
-
+  const [selectedOption, setSelectionOption] = React.useState('one-way');
+  const [startDate, setStartDate] = React.useState(
+    formatDate(new Date(Date.now() + 1000 * 60 * 60 * 24))
+  );
+  const [endDate, setEndDate] = React.useState(startDate);
   return (
-    <div className="transfer-list">
-      <ItemList items={left} setItems={setLeft} />
-      <div className="transfer-list__actions">
-        <button
-          aria-label="transfer left to right all"
-          disabled={right.size === 0}
-          onClick={() => {
-            transferAllItems(right, setRight, left, setLeft);
+    <div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          console.log('asdf');
+          alert(
+            `type: ${selectedOption} date: ${startDate} ${
+              selectedOption === '2-way' && endDate
+            }`
+          );
+        }}
+      >
+        <select
+          id="select"
+          onChange={(e) => {
+            setSelectionOption(e.target.value);
           }}
         >
-          <span aria-hidden={true}>&lt;&lt;</span>
-        </button>
-        <button
-          aria-label="transfer right to left"
-          disabled={hasNoItemSelected(right)}
-          onClick={() => {
-            transferSelectedItem(right, setRight, left, setLeft);
+          <option value="one-way">One Way</option>
+          <option value="2-way">2 way</option>
+        </select>
+        <input
+          type="date"
+          aria-label="start"
+          min={formatDate(new Date())}
+          value={startDate}
+          onChange={(e) => {
+            setStartDate(e.target.value);
           }}
-        >
-          <span aria-hidden={true}>&lt;</span>
-        </button>
-        <button
-          aria-label="transfer left to right"
-          disabled={hasNoItemSelected(left)}
-          onClick={() => {
-            transferSelectedItem(left, setLeft, right, setRight);
-          }}
-        >
-          <span aria-hidden={true}>&gt;</span>
-        </button>
-        <button
-          aria-label="transfer right to left all"
-          disabled={left.size === 0}
-          onClick={() => {
-            transferAllItems(left, setLeft, right, setRight);
-          }}
-        >
-          <span aria-hidden={true}>&gt;&gt;</span>
-        </button>
-      </div>
-      <ItemList items={right} setItems={setRight} />
+        />
+        {selectedOption === '2-way' && (
+          <input
+            type="date"
+            aria-label="return date"
+            value={endDate}
+            min={startDate}
+            onChange={(e) => {
+              setEndDate(e.target.value);
+            }}
+          />
+        )}
+        <button type="submit">Book</button>
+      </form>
     </div>
   );
 }
